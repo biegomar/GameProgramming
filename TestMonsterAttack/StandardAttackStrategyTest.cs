947e0d0cf37f1976d646dc8a7d3371a480a44feb
Marc Biegota <marc.biegota@web.de>
2023-09-24 16:41:06 +0200

    add some tests

--- TestMonsterAttack/StandardAttackStrategyTest.cs
+++ TestMonsterAttack/StandardAttackStrategyTest.cs
@@ -36,5 +36,71 @@ namespace TestMonsterAttack
             Assert.Equal(expected, actual);
 
         }
+
+        [Fact]
+        public void IsFightPossibleTrueTest()
+        {
+            //Arrange
+            var sut = new StandardAttackStrategy();
+            var monster1 = new Monster(sut)
+            {
+                R = MonsterClass.Ork,
+                HP = 120,
+                AP = 10,
+                DP = 10,
+                S = 10
+            };
+
+            var monster2 = new Monster(sut)
+            {
+                R = MonsterClass.Troll,
+                HP = 120,
+                AP = 10,
+                DP = 8,
+                S = 10
+            };
+
+            var expected = true;
+
+            //Act
+            var actual = sut.isFightPossible(monster1, monster2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void IsFightPossibleFalseTest()
+        {
+            //Arrange
+            var sut = new StandardAttackStrategy();
+            var monster1 = new Monster(sut)
+            {
+                R = MonsterClass.Ork,
+                HP = 120,
+                AP = 10,
+                DP = 10,
+                S = 10
+            };
+
+            var monster2 = new Monster(sut)
+            {
+                R = MonsterClass.Troll,
+                HP = 120,
+                AP = 10,
+                DP = 10,
+                S = 10
+            };
+
+            var expected = false;
+
+            //Act
+            var actual = sut.isFightPossible(monster1, monster2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
     }
 }
