fa44d6e7c475cd3b013a81a713f8974012a7ceb3
Marc Biegota <marc.biegota@web.de>
2023-10-14 20:23:11 +0200

    initial commit for maze

---
+++ Mazes/Mazes.Cli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mazes.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Mazes/Mazes.Contracts/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazes.Contracts
+{
+    internal class Cell
+    {
+        private readonly IDictionary<Directions, Cell?> Neighbours = new Dictionary<Directions, Cell?>();
+
+        public Cell(Cell? northernNeighbour = null, Cell? easternNeighbour = null, Cell? southernNeighbour = null, Cell? westernNeighbout = null)
+        {
+            Neighbours.Add(Directions.North, northernNeighbour);
+            Neighbours.Add(Directions.East, easternNeighbour);
+            Neighbours.Add(Directions.South, southernNeighbour);
+            Neighbours.Add(Directions.West, westernNeighbout);
+        }
+    }
+}
