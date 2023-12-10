2ca1389e8a4bb421ea74e587ffb9e11573413716
Marc Biegota <marc.biegota@web.de>
2023-12-10 17:30:22 +0100

    expand fighting system

--- NearlyRogue/NearlyRogue.Core/FightSystems/FightSystem.cs
+++ NearlyRogue/NearlyRogue.Core/FightSystems/FightSystem.cs
@@ -1,21 +1,34 @@
-﻿namespace NearlyRogue.Core.FightSystems;
+﻿using NearlyRogue.Core.Dices;
+using NearlyRogue.Core.Players;
+
+namespace NearlyRogue.Core.FightSystems;
 
 public class FightSystem
 {
-    private readonly Random random;
-    
+    private readonly Random random = new Random();
+    private byte additionalDamage;
+    private byte additionalHit;
+    private IList<DiceThrow> damage = new List<DiceThrow>();
     
     public void BattleOneRound(ICreature attacker, ICreature defender)
     {
-        foreach (var diceThrow in attacker.Damage)
+        if (attacker is Player player)
         {
-            if (DoesSwingHit(attacker, defender,0))
-            {
-                var result = diceThrow.Dice.Roll(diceThrow.Tries);
-            }    
+            this.SetAdditionalDamageAndHit(player);
         }
         
-        
+        foreach (var diceThrow in attacker.Damage)
+        {
+            var strengthCorrector = (byte)(this.additionalHit + this.CalculateStrengthCorrector(attacker.Strength));
+            if (DoesSwingHit(attacker, defender, strengthCorrector))
+            {
+                ushort rollResult = diceThrow.Dice.Roll(diceThrow.Tries);
+
+                ushort attackResult = (ushort)(rollResult + this.additionalDamage + this.CalculateDamageCorrector(attacker.Strength));
+                
+                defender.HitPoints -= Math.Max((ushort)0, attackResult);
+            }
+        }
     }
     
     public sbyte CalculateStrengthCorrector(ushort strength)
@@ -32,11 +45,37 @@ public class FightSystem
         return add;
     }
     
+    public sbyte CalculateDamageCorrector(ushort strength)
+    {
+        sbyte add = 6;
+
+        if (strength < 8) return (sbyte)(strength - 7);
+        
+        if (strength < 31) add--;
+        if (strength < 22) add--;
+        if (strength < 20) add--;
+        if (strength < 18) add--;
+        if (strength < 17) add--;
+        if (strength < 16) add--;
+        
+        return add;
+    }
+    
     private bool DoesSwingHit(ICreature attacker, ICreature defender, byte attackerHitBonus)
     {
         var res = this.random.Next(1,21);
         var need = 20 - attacker.ExperienceLevel - defender.AmorClass;
 
         return res + attackerHitBonus >= need;
+    }
+
+    private void SetAdditionalDamageAndHit(Player attacker)
+    {
+        if (attacker.ActiveWeapon != null)
+        {
+            this.additionalDamage = attacker.ActiveWeapon.AdditionalDamage;
+            this.additionalHit = attacker.ActiveWeapon.AdditionalHit;
+            this.damage = attacker.ActiveWeapon.Damage;
+        }
     }
 }
