b98b6d142691dc1cfe8d93c88048b2550cf8f1eb
Marc Biegota <marc.biegota@web.de>
2024-05-21 20:40:28 +0200

    refactor

---
+++ Mazes/ProceduralContentGeneration.Contracts/BaseDungeonGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Mazes.Contracts
+{
+    public abstract class BaseDungeonGenerator<T> : IProceduralContentGenerator<T>
+    {
+        public abstract IList<Cell<T>> Generate(IList<Cell<T>> cells);
+
+        public IList<Cell<T>> InitializeCells(IList<Cell<T>> cells)
+        {
+            return cells;
+        }
+
+        public IList<Cell<T>> LinkCells(IList<Cell<T>> cells)
+        {
+            return cells;
+        }
+    }
+}
---
+++ Mazes/ProceduralContentGeneration.Contracts/DungeonOfDoomGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Mazes.Contracts
+{
+    public class DungeonOfDoomGenerator<T> : BaseDungeonGenerator<T>
+    {
+        
+        public override IList<Cell<T>> Generate(IList<Cell<T>> cells)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
