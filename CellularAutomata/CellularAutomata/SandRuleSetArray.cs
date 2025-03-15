7c09a5b15be0db0ee35098744d3d96870224031f
Marc Biegota <marc.biegota@web.de>
2025-03-15 16:57:42 +0100

    optimize enum

--- CellularAutomata/CellularAutomata/SandRuleSetArray.cs
+++ CellularAutomata/CellularAutomata/SandRuleSetArray.cs
@@ -153,6 +153,7 @@ public sealed class SandRuleSetArray : IRuleSet<SandCellState>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private bool IsSand(SandCellState cellState)
     {
-        return cellState is SandCellState.Sand or SandCellState.SandDark or SandCellState.SandLight or SandCellState.SandMedium;
+        //return cellState is SandCellState.Sand or SandCellState.SandDark or SandCellState.SandLight or SandCellState.SandMedium;
+        return (byte)cellState > 1;
     }
 }
