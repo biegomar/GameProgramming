c476bfdd96c333d3876e09d9717b9862773744b2
Marc Biegota <marc.biegota@web.de>
2025-03-23 14:19:02 +0100

    small refactoring

--- CellularAutomata/AvaloniaGameOfLive/Program.cs
+++ CellularAutomata/AvaloniaGameOfLive/Program.cs
@@ -19,7 +19,5 @@ class Program
             .UsePlatformDetect()
             .UseSkia()
             .WithInterFont()
-            .LogToTrace()
-        
-            ;
+            .LogToTrace();
 }
