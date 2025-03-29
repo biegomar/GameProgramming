56bad2aab08bb68ed4759f1091ede04759506ef6
biegomar <marc.biegota@web.de>
2025-03-29 17:59:02 +0100

    feature: add HasMoved flag to cell

--- CellularAutomata/CellularAutomata/Cell.cs
+++ CellularAutomata/CellularAutomata/Cell.cs
@@ -2,8 +2,5 @@
 
 namespace CellularAutomata;
 
-[StructLayout(LayoutKind.Sequential, Size = 1)]
-public record struct Cell(CellState State)
-{
-    public CellState State = State;
-}
+[StructLayout(LayoutKind.Sequential, Size = 2)]
+public record struct Cell(CellState State, bool HasMoved = false);
