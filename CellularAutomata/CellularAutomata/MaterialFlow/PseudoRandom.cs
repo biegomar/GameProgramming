3d31ecc8d9160f2c4055508c494ed86377b18b6b
biegomar <marc.biegota@web.de>
2025-04-18 11:46:31 +0200

    feature: new pseudo random class

---
+++ CellularAutomata/CellularAutomata/MaterialFlow/PseudoRandom.cs
@@ -0,0 +1,27 @@
+﻿namespace CellularAutomata.MaterialFlow;
+
+public class PseudoRandom
+{
+    private uint x = 123456789;
+    private uint y = 362436069;
+    private uint z = 521288629;
+    private uint w = 88675123;
+
+    public PseudoRandom(uint seed)
+    {
+        x ^= seed;
+        y ^= seed << 13;
+        z ^= seed >> 9;
+        w ^= seed << 7;
+    }
+    
+    public uint Next(uint max)
+    {
+        var t = x ^ (x << 11);
+        x = y; y = z; z = w;
+        w = w ^ (w >> 19) ^ t ^ (t >> 8);
+        return w % max;
+    }
+    
+    public bool Chance(int percent) => Next(100) < percent;
+}
