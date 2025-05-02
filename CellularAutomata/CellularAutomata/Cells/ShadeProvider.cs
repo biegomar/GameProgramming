64f1bd96471eb13dd1df2b197fb0ddb7e599e866
biegomar <marc.biegota@web.de>
2025-05-02 22:35:09 +0200

    feature: introduce colors for snow, ice and stone, to be consitent

--- CellularAutomata/CellularAutomata/Cells/ShadeProvider.cs
+++ CellularAutomata/CellularAutomata/Cells/ShadeProvider.cs
@@ -12,6 +12,9 @@ public static class ShadeProvider
             CellType.Solid => CellColor.Solid,
             CellType.Sand => (CellColor)random.Next(2, 12),
             CellType.Water => (CellColor)random.Next(12, 22),
+            CellType.Snow => (CellColor)random.Next(22, 32),
+            CellType.Ice => (CellColor)random.Next(32, 42),
+            CellType.Stone => (CellColor)random.Next(42, 52),
             _ => CellColor.Empty
         };
     }
